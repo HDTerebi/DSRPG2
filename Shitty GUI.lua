dd5491e9a814e5107bbd7ca01ccd57ed41c80569
Terebi <69407980+HDTerebi@users.noreply.github.com>
2021-02-26 19:54:48 -0500

    Update Shitty GUI.lua

--- Shitty GUI.lua
+++ Shitty GUI.lua
@@ -1,1 +1,36 @@
-
+--OutDated
+game.StarterGui:SetCore("SendNotification", {
+Title = "Main DSRPG 2 Gui";
+Text = "Outdated Link...",
+Icon = "rbxassetid://5472203252";
+Duration = 3;
+})
+wait(3)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Main DSRPG 2 Gui";
+Text = "Updating, Please Wait...",
+Icon = "rbxassetid://5472203252";
+Duration = 3;
+})
+wait(2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Main DSRPG 2 Gui";
+Text = "Updating.",
+Icon = "rbxassetid://5472203252";
+Duration = 1;
+})
+wait(1)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Main DSRPG 2 Gui";
+Text = "Updating..",
+Icon = "rbxassetid://5472203252";
+Duration = 1;
+})
+wait(1)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Main DSRPG 2 Gui";
+Text = "Updating...",
+Icon = "rbxassetid://5472203252";
+Duration = 1;
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HDTerebi/DSRPG2/master/DSRPG2%20Main%20Gui"))()
