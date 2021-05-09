b68fafb509b546126dd3cd8367da06a081f1dc01
Terebi <69407980+HDTerebi@users.noreply.github.com>
2021-05-09 04:15:34 -0700

    Update Waiting Match.lua

--- Waiting Match.lua
+++ Waiting Match.lua
@@ -1,6 +1,6 @@
 while wait(3) do
 game.StarterGui:SetCore("SendNotification", {
-Title = "Ranked DSRPG 2 Gui";
+Title = "HvZ DSRPG 2 Gui";
 Text = "Waiting For A Match To Start...",
 Icon = "rbxassetid://5472203252";
 Duration = 3;
